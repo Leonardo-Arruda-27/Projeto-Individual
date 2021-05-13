0fce95436c7d63dbc13d9800072ad10ffbee0e59
Leonardo-Arruda-27 <leonardo.goncalves@bandtec.com.br>
2021-05-13 15:51:39 -0300

    fim das ideias do shrek

---
+++ Ideias/shrek/script.js
@@ -0,0 +1,10 @@
+function lord(){
+    alert('PARABÉNS, VOCÊ ACHOU O TÃO TEMIVEL LORD FARQUAAD')
+farquaad.style.display = 'block';
+lorde.style.display = 'block';
+}
+
+function show(){
+    alert('PARABÉNS OGRINHO(A) POR ENCONTRAR O AQUIVO SECRETO 😯');
+    Boa.style.display = 'block';
+}
